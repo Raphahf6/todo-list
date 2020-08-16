698184d5ab571e2bc4ca08276774c31405abd80a
raphahf6 <raphaellima6@gmail.com>
2020-08-16 17:06:25 -0300

    testando

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const divTarefas = document.getElementById('div-tarefas')
 const divapp = document.getElementById('app')
 const btnAddTarefa = document.getElementById('adiciona-tarefa')
 const inputAddTarefa = document.getElementById('nova-tarefa')
-const urlRaphael = 'http://localhost:3000/raphael'
-const urlMensagem = 'http://localhost:3000/mensagems'
+const urlUsuarios = 'https://raphahf6.github.io/todo-list/db.json'
 // Obtém a data/hora atual
 var data = new Date();
 
@@ -32,78 +31,90 @@ var str_hora = hora + ':' + min + ':' + seg;
 // })
 
 
-axios.get(urlRaphael)
+axios.get(urlUsuarios)
     .then(response => {
-        const api = response.data[0]
-        const { id, nome, img, mensagem } = api
-
-        divapp.innerHTML += `<div class="list-group">
+        const api = response.data
+        const { usuario } = api
+        for (i = 0; i < usuario.length; i++) {
+            const usuarioAtual = usuario[i]
+            console.log(usuarioAtual.img)
+            divapp.innerHTML += `<div class="list-group">
                 <a href="#" class="list-group-item list-group-item-action active" id="usuario">
-                  ${nome}
+                  ${usuarioAtual.nome}
                 </a>
 
               </div>`
 
               
 
+
+
+
+        }
+
         const divUsuario = document.getElementById(`usuario`)
 
-        axios.get(urlMensagem)
-            .then(response => {
-                for (i = 0; i < response.data.length; i++) {
-                    const api = response.data[i]
-                    const { mensagem } = api
+        const { mensagems } = api
+        for (i = 0; i < mensagems.length; i++) {
+            const mensagemAtual = mensagems[i]
 
-                    divUsuario.addEventListener('click', () => {
-                        if (divTarefas.childElementCount < response.data.length) {
-                            inputAddTarefa.removeAttribute('disabled')
-                            btnAddTarefa.removeAttribute('disabled')
-                            divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
+            divUsuario.addEventListener('click', () => {
+                if (divTarefas.childElementCount < mensagems.length) {
+                    inputAddTarefa.removeAttribute('disabled')
+                    btnAddTarefa.removeAttribute('disabled')
+                    divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
                                                      <li class="list-group-item form-inline col-xl-12" id="tarefas">
-                                                       <button type="button" class="list-group-item list-group-item-action" id="mensagem-adicionada-${api.id}">${mensagem} <img src="${img}" class="rounded float-right" id="img-${id}" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
+                                                       <button type="button" class="list-group-item list-group-item-action" id="mensagem-adicionada-${api.id}">${mensagemAtual.mensagem} <img src="${usuario[0].img}" class="rounded float-right" id="img-${usuario.id}" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
                                                             </li>
                                                             </ul></div>`
 
-                        }
-
-                        
-                        
-                    })
-
-                   
-
                 }
 
-            
-                btnAddTarefa.addEventListener('click', () => {
-                    axios.post(`${urlMensagem}`, {
-                        "mensagem": `${inputAddTarefa.value} hora: ${str_hora} ${str_data} `
-                    })
-                        .then(response => {
-                            if (divTarefas.childElementCount > 0) {
-                                divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
-                                                         <li class="list-group-item form-inline col-xl-12" id="tarefas">
-                                                           <button type="button" class="list-group-item list-group-item-action" id="mensagem-adicionada-${divTarefas.childElementCount +1}">${inputAddTarefa.value} hora: ${str_hora} ${str_data}<img src="${img}" class="rounded float-right" id="img-${id}" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
-                                                                </li>
-                                                                </ul></div>`
-                            } 
 
-
-
-                        })
-                        .catch(err => {
-                            console.log(err)
-                        })
-                })
-
-                
-            
-
-                
 
             })
 
-           
+
+
+        }
+
+
+        btnAddTarefa.addEventListener('click', () => {
+            axios.post(`${urlUsuarios}`, {
+                "mensagem": `${inputAddTarefa.value} hora: ${str_hora} ${str_data} `
+            })
+                .then(response => {
+                    if (divTarefas.childElementCount > 0) {
+                        divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
+                                                         <li class="list-group-item form-inline col-xl-12" id="tarefas">
+                                                           <button type="button" class="list-group-item list-group-item-action" id="mensagem-adicionada-${divTarefas.childElementCount + 1}">${inputAddTarefa.value} hora: ${str_hora} ${str_data}<img src="${usuario[0].img}" class="rounded float-right" id="img-${usuario[0].id}" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
+                                                                </li>
+                                                                </ul></div>`
+                    }
+
+
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+
+
+            // axios.get(urlUsuarios)
+            //     .then(response => {
+            //         const api = response.data
+
+            //     })
+
+
+
+
+
+
+        })
+
+
 
 
 
