e5104b9be8022879f3e606f933d28fa56bb9a030
raphahf6 <raphaellima6@gmail.com>
2021-05-06 23:23:42 -0300

    adiciona tela de login

--- main.js
+++ main.js
@@ -1,106 +1,99 @@
-const divTarefas = document.getElementById('div-tarefas')
-const divRepositorios = document.getElementById('div-repositorios')
-const divapp = document.getElementById('app')
-const btnAddTarefa = document.getElementById('adiciona-tarefa')
-const inputAddTarefa = document.getElementById('nova-tarefa')
-const btnDark = document.querySelector('button.dark')
-const urlUsuarios = 'http://localhost:3000/usuario'
-const urlTodo = 'https://raphahf6.github.io/todo-list/db.json'
-const urlGit = 'https://api.github.com/users/raphahf6'
+const sectionApp = document.getElementById('app-main')
+const sectionLogin = document.getElementById('login')
+sectionApp.setAttribute('hidden', '')
+const inputLogin = document.getElementById('input-login')
+const bntLogin = document.getElementById('btn-login')
 
-// Obtém a data/hora atual
-var data = new Date();
+bntLogin.addEventListener('click', () => {
+    const urlGit = `https://api.github.com/users/${inputLogin.value}`
 
-// Guarda cada pedaço em uma variável
-var dia = data.getDate();           // 1-31
-var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-var mes = data.getMonth();          // 0-11 (zero=janeiro)
-var ano2 = data.getYear();           // 2 dígitos
-var ano4 = data.getFullYear();       // 4 dígitos
-var hora = data.getHours();          // 0-23
-var min = data.getMinutes();        // 0-59
-var seg = data.getSeconds();        // 0-59
-var mseg = data.getMilliseconds();   // 0-999
-var tz = data.getTimezoneOffset(); // em minutos
+    axios.get(urlGit)
+        .then(response => {
+            const { avatar_url, bio, name, blog, repos_url } = response.data
 
-// Formata a data e a hora (note o mês + 1)
-var str_data = dia + '/' + (mes + 1) + '/' + ano4;
-var str_hora = hora + ':' + min + ':' + seg;
+            if (name == null) {
+                alert('Usuario invalido')
+                location.reload()
+            }
+            sectionApp.removeAttribute('hidden')
+            sectionLogin.setAttribute('hidden', '')
+            const divTarefas = document.getElementById('div-tarefas')
+            const divRepositorios = document.getElementById('div-repositorios')
+            const divapp = document.getElementById('app')
+            const btnAddTarefa = document.getElementById('adiciona-tarefa')
+            const inputAddTarefa = document.getElementById('nova-tarefa')
+            const btnDark = document.querySelector('button.dark')
+            const urlUsuarios = 'http://localhost:3000/usuario'
+            const urlTodo = 'https://raphahf6.github.io/todo-list/db.json'
 
+            const nomeUsuario = document.getElementById('nome-titulo')
+            const bioUsuario = document.getElementById('bio')
+            const imgUsuario = document.getElementById('img-usuario')
+            imgUsuario.setAttribute('src', `${avatar_url}`)
+            nomeUsuario.textContent = `${name}`
+            if (bio == null) {
+                bioUsuario.setAttribute('hidden', '')
+            }
+            btnDark.addEventListener('click', () => {
+                document.getElementById('perfil').setAttribute('style', 'background-color: black;')
+                let colorir = document.querySelectorAll('.txt-usuario')
+                colorir.forEach(element => {
+                    element.setAttribute('style', 'color:white;')
+                })
 
-btnDark.addEventListener('click', () => {
-    document.getElementById('perfil').setAttribute('style', 'background-color: black;')
-    let colorir = document.querySelectorAll('.txt-usuario')
-    colorir.forEach(element => {
-        element.setAttribute('style', 'color:white;')
-    })
+            })
 
-})
+            bioUsuario.textContent = `${bio}`
 
+            axios.get(repos_url)
+                .then(res => {
 
-axios.get(urlGit)
-    .then(response => {
-        const { avatar_url, bio, name, blog, repos_url } = response.data
-        const nomeUsuario = document.getElementById('nome-titulo')
-        const bioUsuario = document.getElementById('bio')
-        const imgUsuario = document.getElementById('img-usuario')
-        imgUsuario.setAttribute('src', `${avatar_url}`)
-        nomeUsuario.textContent = `${name}`
-        bioUsuario.textContent = `${bio}`
-
-
-
-
-
-        axios.get(repos_url)
-            .then(res => {
-
-                for (i = 0; i < res.data.length; i++) {
-                    const { name } = res.data[i]
-                    const bntRepos = document.getElementById('repositorios')
-                    // const downloadLink = `https://github.com/raphahf6/${name}/archive/master.zip`
-                    bntRepos.addEventListener('click', () => {
-                        inputAddTarefa.setAttribute('hidden', '')
-                        btnAddTarefa.setAttribute('hidden', '')
-                        divRepositorios.removeAttribute('hidden')
-                        divTarefas.setAttribute('hidden', '')
-                        if (divRepositorios.childElementCount < res.data.length) {
-                            divRepositorios.innerHTML += `<div class="list-group">
+                    for (i = 0; i < res.data.length; i++) {
+                        const { name } = res.data[i]
+                        const bntRepos = document.getElementById('repositorios')
+                        // const downloadLink = `https://github.com/raphahf6/${name}/archive/master.zip`
+                        bntRepos.addEventListener('click', () => {
+                            inputAddTarefa.setAttribute('hidden', '')
+                            btnAddTarefa.setAttribute('hidden', '')
+                            divRepositorios.removeAttribute('hidden')
+                            divTarefas.setAttribute('hidden', '')
+                            if (divRepositorios.childElementCount < res.data.length) {
+                                divRepositorios.innerHTML += `<div class="list-group">
                             <a href="#" id="repositorio" class="list-group-item list-group-item-action active repos" id="">
                               ${name} <img src="${avatar_url}" class="rounded float-right" alt="..." style="width: 33px; height:33px;">
                             </a>
                             
                           </div>`
 
-                            const repositorioAtual = document.querySelectorAll(`a#repositorio`)
-                            repositorioAtual.forEach(repo => {
-                                // repo.setAttribute('href', `${downloadLink}`)
-                            })
-                        }
+                                const repositorioAtual = document.querySelectorAll(`a#repositorio`)
+                                repositorioAtual.forEach(repo => {
+                                    // repo.setAttribute('href', `${downloadLink}`)
+                                })
+                            }
 
 
-                    })
+                        })
 
-                }
-                axios.get(urlTodo)
-                    .then(response => {
-                        const api = response.data
-                        const { todo } = api
-                        if (response.data.length == 0) {
-                            btnAddTarefa.removeAttribute('hidden')
-                            inputAddTarefa.removeAttribute('hidden')
-                        }
-                        for (j = 0; j < todo.length; j++) {
+                    }
+                    axios.get(urlTodo)
+                        .then(response => {
+                            const api = response.data
+                            const { todo } = api
+                            if (response.data.length == 0) {
+                                btnAddTarefa.removeAttribute('hidden')
+                                inputAddTarefa.removeAttribute('hidden')
+                            }
+                            for (j = 0; j < todo.length; j++) {
 
-                            const { mensagem, id } = todo[j]
+                                const { mensagem, id } = todo[j]
 
-                            const btnTarefas = document.getElementById(`btn-tarefas`)
+                                const btnTarefas = document.getElementById(`btn-tarefas`)
 
 
-                            inputAddTarefa.removeAttribute('hidden')
-                            btnAddTarefa.removeAttribute('hidden')
+                                inputAddTarefa.removeAttribute('hidden')
+                                btnAddTarefa.removeAttribute('hidden')
 
-                            divTarefas.innerHTML += `<div class="container col-xl-12" id="mensagem-${id}"><ul class="list-group form-inline col-xl-12">
+                                divTarefas.innerHTML += `<div class="container col-xl-12" id="mensagem-${id}"><ul class="list-group form-inline col-xl-12">
                                                              <li class="mensagem-${id} list-group-item form-inline col-xl-12 bg-transparent" id="tarefas" style="border-style:none;">
                                                                <button type="text" class="tarefa list-group-item list-group-item-action" id="${id}">${mensagem}<img src="${avatar_url}" class="rounded float-right" id="img-${id}" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
                                                                     </li>
@@ -110,62 +103,62 @@ axios.get(urlGit)
 
 
 
-                            let tarefa = document.querySelectorAll('button.tarefa')
+                                let tarefa = document.querySelectorAll('button.tarefa')
 
 
 
-                            Array.of(tarefa).map((btn, index) => {
-                                addEventListener('click', () => {
-                                    const msgRemovida = document.getElementById(`${btn.id}`)
-                                    const divMensagem = document.querySelector(`div#mensagem-${btn.id}`)
-                                    const liMensagem = document.querySelector(`li.mensagem-${btn.id}`)
+                                Array.of(tarefa).map((btn, index) => {
+                                    addEventListener('click', () => {
+                                        const msgRemovida = document.getElementById(`${btn.id}`)
+                                        const divMensagem = document.querySelector(`div#mensagem-${btn.id}`)
+                                        const liMensagem = document.querySelector(`li.mensagem-${btn.id}`)
 
-                                   /* if (divTarefas.innerText.length > 35) {
-                                        axios.delete(`${urlTodo}/${btn.id}`, {
+                                        /* if (divTarefas.innerText.length > 35) {
+                                             axios.delete(`${urlTodo}/${btn.id}`, {
+     
+                                             })
+                                                 .then(res => {
+                                                     liMensagem.removeChild(msgRemovida)
+                                                     divMensagem.setAttribute('hidden', '')
+                                                 })
+                                                 .catch(err => {
+                                                     console.log(err)
+                                                 })
+     
+                                         }*/
 
-                                        })
-                                            .then(res => {
-                                                liMensagem.removeChild(msgRemovida)
-                                                divMensagem.setAttribute('hidden', '')
-                                            })
-                                            .catch(err => {
-                                                console.log(err)
-                                            })
-
-                                    }*/
-
+                                    })
                                 })
-                            })
-                            tarefa.forEach(btn => {
-                                btn.addEventListener('click', () => {
+                                tarefa.forEach(btn => {
+                                    btn.addEventListener('click', () => {
 
 
 
+                                    })
                                 })
-                            })
 
 
 
 
-                            btnTarefas.addEventListener('click', () => {
-                                inputAddTarefa.removeAttribute('hidden')
-                                btnAddTarefa.removeAttribute('hidden')
-                                divRepositorios.setAttribute('hidden', '')
-                                if (divTarefas.childElementCount < response.data.lengt || divRepositorios.hasAttribute('hidden')) {
+                                btnTarefas.addEventListener('click', () => {
+                                    inputAddTarefa.removeAttribute('hidden')
+                                    btnAddTarefa.removeAttribute('hidden')
+                                    divRepositorios.setAttribute('hidden', '')
+                                    if (divTarefas.childElementCount < response.data.lengt || divRepositorios.hasAttribute('hidden')) {
 
-                                    divTarefas.removeAttribute('hidden')
-
-
-                                }
-                            })
+                                        divTarefas.removeAttribute('hidden')
 
 
+                                    }
+                                })
 
 
-                        }
 
-                        btnAddTarefa.addEventListener('click', () => {
-                            divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
+
+                            }
+
+                            btnAddTarefa.addEventListener('click', () => {
+                                divTarefas.innerHTML += `<div class="container col-xl-12"><ul class="list-group form-inline col-xl-12">
                                                      <li class="list-group-item form-inline col-xl-12 bg-transparent" id="tarefas" style="border-style:none;">
                                                        <button type="button" class="btns list-group-item list-group-item-action" id="${divTarefas.childElementCount}">${inputAddTarefa.value}<img src="${avatar_url}" class="rounded float-right" id="img" alt="..." style="width: 50px; height:50px;margin-right: 9px;"></button>
                                                             </li>
@@ -173,21 +166,36 @@ axios.get(urlGit)
 
 
 
-                            inputAddTarefa.value = ''
+                                inputAddTarefa.value = ''
 
 
 
+
+
+
+                            })
 
 
 
                         })
+                })
+
+
+        })
 
 
 
-                    })
-            })
+})
 
 
-    })
+
+
+
+
+
+
+
+
+
 
 
